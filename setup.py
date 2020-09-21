2a394ddce2325647f3f90bad25a31b588ee5c483
IKiselevii <newkis710@yandex.ru>
2020-09-21 11:00:13 +0300

    Try to fix error with install packages

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='graphicone_graphs_compiler',
     url='https://github.com/trendvision/graphicone_graphs_compiler',
     packages=['graphicone_graphs_compiler'],
-    dependency_link=[
+    install_requires=[
+        'sqlalchemy'
         'git+https://github.com/trendvision/graphicone_models.git#egg=graphicone_models',
         'git+https://github.com/trendvision/graphicone_social_relations.git#egg=graphicone_social_relations'
     ],
-    install_requires=['sqlalchemy', 'graphicone_models', 'graphicone_social_relations'],
     version='0.1.1',
     license='TRV',
     description='creation graphs models',
