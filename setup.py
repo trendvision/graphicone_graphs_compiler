ab98d15cedc1a3b338d613d1f22340647d417465
IKiselevii <newkis710@yandex.ru>
2020-09-21 11:08:58 +0300

    Try to fix error with install packages

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         'sqlalchemy'
         'graphicone_models @ git+https://github.com/trendvision/graphicone_models.git@v0.1#egg=graphicone_models',
-        'graphicone_social_relations @ git+https://github.com/trendvision/graphicone_social_relations.git@v0.1.1#egg=graphicone_social_relations'
+        'graphicone_social_relations @ git+https://github.com/trendvision/graphicone_social_relations.git@v0.1#egg=graphicone_social_relations'
     ],
     version='0.1.1',
     license='TRV',
